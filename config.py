d8007b8c32e02399ff96e2f027904ee82053dacc
biplobdeveloper965-cmd <biplobdeveloper965@gmail.com>
2026-03-08 03:19:13 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+TOKEN = "7548520925:AAEstjmiMIjkErT91k0-Kt6XxpGaDRe1CBg"
+
+ADMIN_IDS = [7666264257]
+
+REF_BONUS = 10
+MIN_WITHDRAW = 100
+
+CHANNELS = [
+    ("Channel 1", "https://t.me/vairalvideoo2"),
+    ("Channel 2", "https://t.me/xxxxxvideo01"),
+    ("Channel 3", "https://t.me/livecall24"),
+    ("Channel 4", "https://t.me/privatevideo0q2")
+]
