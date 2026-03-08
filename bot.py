325c1c31c5399d6ecf19208fa1f5b802fbab43ee
biplobdeveloper965-cmd <biplobdeveloper965@gmail.com>
2026-03-08 05:16:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,9 +5,7 @@ import database
 import admin_commands
 import user_commands
 
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
     user = update.effective_user.id
 
     database.cursor.execute(
@@ -31,10 +29,56 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         reply_markup=keyboard
     )
 
-
 async def verify(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
     query = update.callback_query
+    user = query.from_user.id
+
+    joined = True
+
+    for title, link in config.CHANNELS:
+        channel = link.replace("https://t.me/", "@")
+
+        try:
+            member = await context.bot.get_chat_member(channel, user)
+
+            if member.status == "left":
+                joined = False
+
+        except:
+            joined = False
+
+    if joined:
+
+        menu = [
+            ["🔥 Trending", "💎 Exclusive"],
+            ["🔗 Invite & Earn", "💰 My Balance"],
+            ["💸 Withdraw"]
+        ]
+
+        reply = ReplyKeyboardMarkup(menu, resize_keyboard=True)
+
+        await query.message.reply_text(
+            "✅ Verified Successfully",
+            reply_markup=reply
+        )
+
+    else:
+        await query.answer("❌ Join all channels first!", show_alert=True)
+
+
+app = ApplicationBuilder().token(config.TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CallbackQueryHandler(verify, pattern="verify"))
+
+app.add_handler(CommandHandler("stats", admin_commands.stats))
+app.add_handler(CommandHandler("broadcast", admin_commands.broadcast))
+
+app.add_handler(CommandHandler("balance", user_commands.balance))
+app.add_handler(CommandHandler("invite", user_commands.invite))
+app.add_handler(CommandHandler("withdraw", user_commands.withdraw))
+
+app.run_polling()    query = update.callback_query
     user = query.from_user.id
 
     joined = True
